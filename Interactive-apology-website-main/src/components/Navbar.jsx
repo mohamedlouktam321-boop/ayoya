379118c9fda6a8981b98bea66e6aae81299ae96c
mohamedlouktam321-boop <mohamedlouktam321@gmail.com>
2026-05-23 14:47:27 +0100

    Add files via upload

---
+++ Interactive-apology-website-main/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav className="w-full px-8 py-4 flex items-center justify-between bg-white/40 backdrop-blur-md shadow-sm">
+      <h1 className="text-2xl font-semibold text-rose-600">
+        Szivii 💗
+      </h1>
+
+      <p className="text-lg text-purple-600 font-medium">
+        Love you cutiee 💕
+      </p>
+    </nav>
+  );
+};
+
+export default Navbar;
