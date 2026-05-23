6798e97d6893f674329e374c7280cd1bb30bd5c4
mohamedlouktam321-boop <mohamedlouktam321@gmail.com>
2026-05-23 14:51:24 +0100

    Add files via upload

---
+++ src/data/story.js
@@ -0,0 +1,28 @@
+import img1 from "../assets/images/img1.jpg";
+import img2 from "../assets/images/img2.jpg";
+import img3 from "../assets/images/img3.jpg";
+import img4 from "../assets/images/img4.jpg";
+import img5 from "../assets/images/img5.jpg";
+
+export const story = [
+  {
+    image: img1,
+    text: "Hiiii Goluuuu, Just wanted to tell you somethingggg...",
+  },
+  {
+    image: img2,
+    text: "Do you remember this picture? Our first picture that you took ✨",
+  },
+  {
+    image: img3,
+    text: "Our first trip as a hackathon... 🥺",
+  },
+  {
+    image: img4,
+    text: "A day full of enjoyymenttt 😌",
+  },
+  {
+    image: img5,
+    text: "Yeah, sometimes I do things that make you angry, and I am sorry for that. But just like this image, I choose US. So maaf kar de goluu ❤️",
+  },
+];
